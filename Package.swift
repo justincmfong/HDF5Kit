4ff085ce0c754aad96b9202addde753c642061c7
Justin <49131879+justincmfong@users.noreply.github.com>
2024-10-23 15:35:25 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["HDF5Kit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/trueb2/CHDF5.git", from: "1.0.0")
+        .package(url: "https://github.com/justincmfong/CHDF5.git", from: "1.0.0")
     ],
     targets: [
         .target(
